3f4bc29f7cfe378846d36028acd078a367aee039
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-02-23 09:48:27 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const bot = new Telegraf('8166477747:AAEB6ZC5v5j6zLmvj5CIPYLehm7PS5hMY4E')
 
 bot.start((ctx) => ctx.reply('Welcome to my first bot'))
 
-bot.command('aur', (ctx) => ctx.reply('Sab badhiya bhai, tu bata?'))
+// bot.command('', (ctx) => ctx.reply(''))
 
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+// respond to normal chat
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears('aur bhai kya karra hai?', (ctx) => ctx.reply('Sab badhiya bhai, tu bata?'))
 bot.launch()
 
